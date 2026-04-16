80bf8e542a80736818178abbbdc79c323109ba9c
debishree <debishree1833@gmail.com>
2026-04-16 20:40:43 +0530

    1st pattern program

---
+++ demo/bin/debs/FisrtPattern.java
@@ -0,0 +1,23 @@
+package demo;
+import java.util.Scanner;
+
+public class FisrtPattern {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a number to print the pattern:");
+		int n = sc.nextInt();
+		pattern(n);
+		
+	}
+	public static void pattern(int n) {
+		for(int i = 1 ; i <= n ; i++) {
+			for(int j = 1 ; j <= n ; j++) {
+				if(i == 1 || j == 1 || i == n || j == n)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
